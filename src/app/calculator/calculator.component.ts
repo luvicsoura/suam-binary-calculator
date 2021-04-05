470619e4162d7e1f5eb18e586f7f70de01cb7300
Victor Oliveira <victoroliveira.dev@gmail.com>
2021-04-05 17:32:03 -0300

    Feat(CalculatorComponent): Integrates functionality to the template

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CalculatorService } from './calculator.service';
 
+const DEFAULT_CURRENT_INPUT = '0';
+
 @Component({
   selector: 'app-calculator',
   providers: [CalculatorService],
@@ -9,11 +11,73 @@ import { CalculatorService } from './calculator.service';
 })
 export class CalculatorComponent implements OnInit {
 
+  result: string;
+
+  private inputs: string[] = [];
+  private currentInput: string;
+
   constructor(
     private calculatorService: CalculatorService
-  ) { }
+  ) {
+    this.resetInput();
+    this.resetResult();
+  }
 
   ngOnInit(): void {
+  }
+
+  insert(input: string) {
+    this.currentInput = this.sanitizeInput(
+      this.currentInput.concat(input)
+    );
+
+    this.result = this.currentInput;
+  }
+
+  confirmNumber = () => {
+
+    this.inputs.push(this.currentInput);
+    this.resetInput();
+    this.resetResult();
+  }
+
+  evaluate = () => {
+    const result = this.calculatorService.add(...this.inputs);
+    console.log({result})
+    this.result = result;
+  }
+
+  clear = () => {
+    this.resetInput();
+    this.resetResult();
+    this.resetInputs();
+  }
+
+  private resetInput() {
+    this.currentInput = DEFAULT_CURRENT_INPUT.slice();
+  }
+
+  private resetResult() {
+    this.result = DEFAULT_CURRENT_INPUT.slice();
+  }
+
+  private resetInputs() {
+    this.inputs = [];
+  }
+
+  private sanitizeInput(input) {
+
+    let sanitizedInput = this.removeInputLeadingZero(input);
+    return sanitizedInput;
+  }
+
+  private removeInputLeadingZero(input) {
+
+    if (this.currentInput[0] === '0') {
+      return  input.slice(1, this.currentInput.length + 1);
+    }
+
+    return input;
   }
 
 }
