6f4433927554e1c70cc4cc1397ea00c42d48a2aa
Victor Oliveira <victoroliveira.dev@gmail.com>
2021-04-05 16:55:06 -0300

    Feat(Calculator): Adds calculator template

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculatorService } from './calculator.service';
 
 @Component({
   selector: 'app-calculator',
+  providers: [CalculatorService],
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private calculatorService: CalculatorService
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculatorComponent } from './calculator.component';
-
-
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @NgModule({
   declarations: [CalculatorComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonsModule
   ]
 })
 export class CalculatorModule { }
